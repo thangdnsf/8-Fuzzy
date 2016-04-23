f7362c74dcf952957deb7dae4734c467b970a4dd
Thang Nguyen <thangdn.tlu@outlook.com>
2016-04-23 21:07:56 +0700

    Update searchTree.cpp

--- 8_Puzzle/8_Puzzle/searchTree.cpp
+++ 8_Puzzle/8_Puzzle/searchTree.cpp
@@ -1,3 +1,19 @@
+/**********************************************************************
+* 8-Fuzzy.
+* version: 1.5
+* Game 8-Fuzzy using A* algorithm
+*
+* November, 25th, 2015
+*
+* This lib was written by DucThang
+* Contact:thangdn.tlu@outlook.com
+*
+* Every comment would be appreciated.
+*
+* If you want to use parts of any code of mine:
+* let me know and
+* use it!
+**********************************************************************/
 #include "searchTree.h"
 
 searchTree::searchTree() {
