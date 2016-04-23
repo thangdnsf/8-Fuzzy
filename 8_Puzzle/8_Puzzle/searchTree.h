4c7529bbd30d9e47767208c702be0ec5adbe318a
Thang Nguyen <thangdn.tlu@outlook.com>
2016-04-23 21:06:57 +0700

    Update searchTree.h

--- 8_Puzzle/8_Puzzle/searchTree.h
+++ 8_Puzzle/8_Puzzle/searchTree.h
@@ -1,9 +1,9 @@
 /**********************************************************************
-* 8-Fuzzy library.
+* 8-Fuzzy.
 * version: 1.5
 * Game 8-Fuzzy using A* algorithm
 *
-* July, 5th, 2015
+* November, 25th, 2015
 *
 * This lib was written by DucThang
 * Contact:thangdn.tlu@outlook.com
